f5c35d6bb48b1425eb9a30eae06fe1a764675c63
Yuhan Si <yuhansi@usc.edu>
2015-11-16 21:47:57 -0800

    add file for scavenger hunt demo

---
+++ newproject/ItemsManager.swift
@@ -0,0 +1,46 @@
+//
+//  ItemsManager.swift
+//  newproject
+//
+//  Created by Hans Si on 11/12/15.
+//  Copyright © 2015 Hans Si. All rights reserved.
+//
+
+import UIKit
+
+class ItemsManager {
+    var itemsList = [ScavengerHuntItem]()
+    
+    func archivePath() -> String? {
+        let directoryList = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        if let documentsPath = directoryList.first {
+            return documentsPath + "/ScavengerHuntItems"
+        }
+        assertionFailure("Could not determine where to save file.")
+        return nil
+    }
+    
+    func save() {
+        if let theArchivePath = archivePath() {
+            if NSKeyedArchiver.archiveRootObject(itemsList, toFile: theArchivePath) {
+                print("Saved successfully.")
+            }
+            else {
+                assertionFailure("Could not save data to \(theArchivePath)")
+            }
+        }
+    }
+    
+    func unarchiveSavedItems() {
+        if let theArchivePath = archivePath() {
+            if NSFileManager.defaultManager().fileExistsAtPath(theArchivePath) {
+                itemsList = NSKeyedUnarchiver.unarchiveObjectWithFile(theArchivePath) as! [ScavengerHuntItem]
+            }
+        }
+    }
+    
+    init() {
+        unarchiveSavedItems()
+    }
+}
+
